66bf01cb5941ce1f36996edd95d595570dfdd6dd
Zishan <50874919+MohammedZ666@users.noreply.github.com>
2025-09-05 17:58:20 +0600

    Update colab_setup.sh

--- colab_setup.sh
+++ colab_setup.sh
@@ -1,4 +1,9 @@
 pip install condacolab
 python3 -c "import condacolab; condacolab.install()"
+conda create -n fouriscale python=3.8
+conda init
+. ~/.bashrc
+conda activate fouriscale
+pip install -r requirements.txt
 mkdir /content/logs
 echo "Colab is ready for fouriscale"  
